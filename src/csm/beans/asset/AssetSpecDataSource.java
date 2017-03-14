4a2e16b1a451a334e229898f74595f55963df1ba
AMilko <AMilko@computel.ru>
2017-03-14 13:24:51 +0300

    Added line testing2 again commit

--- src/csm/beans/asset/AssetSpecDataSource.java
+++ src/csm/beans/asset/AssetSpecDataSource.java
@@ -1,6 +1,8 @@
 package csm.beans.asset;
 
 import psdi.util.MXException;
+import psdi.util.logging.MXLogger;
+import psdi.util.logging.MXLoggerFactory;
 import psdi.webclient.system.beans.DataBean;
 
 import java.rmi.RemoteException;
@@ -9,8 +11,15 @@ import java.rmi.RemoteException;
  * Created by AMilko on 14.03.2017.
  */
 public class AssetSpecDataSource extends DataBean {
+
+
+    MXLogger custLogger = MXLoggerFactory.getLogger("maximo.custClass01");
+
     @Override
     protected void initialize() throws MXException, RemoteException {
+        custLogger.debug("testing");
+        custLogger.debug("testing2");
+
         super.initialize();
         this.getMboSet().setUserWhere("section='1'");
     }
